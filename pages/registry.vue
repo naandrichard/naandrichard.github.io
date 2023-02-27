c9fcb6f03cfb29f63391d2b513ea6213083b5421
Richard <heyrichardshi@users.noreply.github.com>
2023-02-26 23:36:58 -0500

    Made registry page responsive.

--- pages/registry.vue
+++ pages/registry.vue
@@ -8,17 +8,17 @@ onMounted(() => {
     <div>
         <Navigation />
 
-        <div class="mt-18 py-8 px-40 w-full text-center">
+        <div class="mt-18 py-8 px-4 lg:px-40 w-full text-center">
             <h1 class="font-serif font-medium text-5xl leading-loose">Registry</h1>
-            <div class="grid grid-cols-5 gap-8">
-                <span class="col-start-2 col-span-3">
+            <div class="grid grid-cols-1 lg:grid-cols-5 gap-8">
+                <span class="lg:col-start-2 lg:col-span-3">
                     Hello friends and family, we’re excited to see you on our big day! In lieu of traditional gifts, we’ve
                     opted
                     for a house and honeymoon fund registry to kickstart the next chapter of our lives. If you’re able, we
                     are
                     so grateful for your contribution to this dream of ours!
                 </span>
-                <span class="col-start-2 col-span-3">
+                <span class="lg:col-start-2 lg:col-span-3">
                     While you can contribute through our online registry, we will also have a wishing well card box at our
                     reception if you’d prefer to give a gift in-person (and avoid the transaction fee).
 
