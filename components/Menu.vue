f81ca855c134eec411c8b04ebe469267cb4659bd
Richard <heyrichardshi@users.noreply.github.com>
2023-02-28 22:35:23 -0500

    Added dinner menu modal to main page.

---
+++ components/Menu.vue
@@ -0,0 +1,79 @@
+<script setup lang="ts">
+const dishes = ref([
+    {
+        english: "Roasted Whole Suckling Pig",
+        chinese: "江南脆皮乳猪全體",
+    },
+    {
+        english: "Sautéed Dried Squid, Mussel & Greens in XO Sauce",
+        chinese: "XO醬甜豆九龍吊片海蚌",
+    },
+    {
+        english: "Cantonese Boiled Live Shrimp",
+        chinese: "白灼游水蝦 (籠子上)",
+    },
+    {
+        english: "Imperial Bird’s Nest Soup with Crabmeat Soup",
+        chinese: "極品蟹肉官燕羹",
+    },
+    {
+        english: "Whole Abalone with Black Mushroom and Sea Cucumber",
+        chinese: "碧綠豪皇原隻鮑魚海參花菇",
+    },
+    {
+        english: "Baked Half Lobsters with Cheese",
+        chinese: "意大利芝士焗開邊龍蝦",
+    },
+    {
+        english: "Forever Luck Crispy Chicken",
+        chinese: "鴻運脆皮炸子雞",
+    },
+    {
+        english: "Steamed Whole Crab & Dried Scallop in Glutinous Rice",
+        chinese: "瑤柱香糯蒸羔蟹",
+    },
+    {
+        english: "French Style Sheraton Steak",
+        chinese: "法式焗牛扒",
+    },
+    {
+        english: "Steamed Seasonal Whole Fish",
+        chinese: "清蒸游水大海斑",
+    },
+    {
+        english: "E-Fu Noodle with Dry Scallop and Mushroom",
+        chinese: "瑤柱金菇伊麵",
+    },
+    {
+        english: "Eight Treasure Rice Pudding",
+        chinese: "八寶甜飯",
+    },
+    {
+        english: "Fresh Fruit Platter",
+        chinese: "合時果盤",
+    },
+    {
+        english: "Sodas & Tea",
+        chinese: "汽水、茶",
+    },
+])
+</script>
+
+<template>
+    <div class="w-full grid grid-cols-2 gap-8 text-center">
+        <div class="col-span-2">
+            <h2 class="font-serif font-medium text-5xl text-center leading-loose mb-8">Dinner</h2>
+            <p class="mb-4">
+                Dinner will be served as a set menu of the following courses.
+            </p>
+            <p>
+                For our friends and family with dietary restrictions, please let us know on your RSVP and we will coordinate alternative dishes.
+            </p>
+        </div>
+        <div class="col-span-2"
+             v-for="dish in dishes">
+            <span class="block">{{ dish.english }}</span>
+            <span>{{ dish.chinese }}</span>
+        </div>
+    </div>
+</template>
--- components/Schedule.vue
+++ components/Schedule.vue
@@ -1,5 +1,43 @@
-<template>
+<script setup lang="ts">
+import { Ref } from 'vue';
 
+const menu = ref(null) as unknown as Ref<HTMLElement>;
+const closeMenuIcon = ref(null) as unknown as Ref<HTMLElement>;
+
+const openMenuClasses = [
+    "opacity-100",
+    "scale-100",
+];
+const closedMenuClasses = [
+    "opacity-0",
+    "scale-0",
+];
+
+function openMenu() {
+    const classes = new Set(menu.value.className.split(" "));
+    const iconClasses = new Set(closeMenuIcon.value.className.split(" "));
+
+    openMenuClasses.forEach(c => { classes.add(c); iconClasses.add(c) });
+    closedMenuClasses.forEach(c => { classes.delete(c); iconClasses.delete(c) });
+
+    menu.value.className = [...classes].join(' ');
+    closeMenuIcon.value.className = [...iconClasses].join(' ');
+}
+
+function closeMenu() {
+    const classes = new Set(menu.value.className.split(" "));
+    const iconClasses = new Set(closeMenuIcon.value.className.split(" "));
+
+    openMenuClasses.forEach(c => { classes.delete(c); iconClasses.delete(c) });
+    closedMenuClasses.forEach(c => { classes.add(c); iconClasses.add(c) });
+    
+    menu.value.className = [...classes].join(' ');
+    closeMenuIcon.value.className = [...iconClasses].join(' ');
+}
+
+</script>
+
+<template>
     <div class="my-12">
         <h2 id="schedule" class="font-serif font-medium text-5xl text-center leading-loose mb-8 scroll-mt-18">Schedule
         </h2>
@@ -26,6 +64,20 @@
             <div class="text-left">
                 <span class="font-bold text-slate-400">7:00 pm</span>
             </div>
+            <div class="text-center col-span-2 relative -top-8">
+                <span class="cursor-pointer underline" @click="openMenu">Click here to see the dinner menu!</span>
+            </div>
         </div>
+    </div>
+
+    <div ref="menu"
+         class="fixed top-20 left-4 right-4 bottom-4 bg-white z-50 p-4 shadow-lg shadow-gray-500 overflow-y-scroll transition-all duration-500 opacity-0 scale-0 border-t border-gray-300 rounded-sm">
+        <Menu />
+    </div>
+    <div ref="closeMenuIcon"
+         class="fixed top-20 left-4 m-1 w-12 h-12 bg-white float-left cursor-pointer transition-all duration-500 opacity-0 scale-0 z-50 origin-[200%_200%]"
+         @click="closeMenu">
+        <div class="w-8 h-1 bg-black absolute top-1/2 left-1/2 -translate-x-1/2 rotate-45"></div>
+        <div class="w-8 h-1 bg-black absolute top-1/2 left-1/2 -translate-x-1/2 -rotate-45"></div>
     </div>
 </template>
