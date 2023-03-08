c0403ae1fe8aba7db98b5413e1e7ea73284f0a0b
Richard <heyrichardshi@users.noreply.github.com>
2023-03-07 20:26:50 -0500

    Fixed issue where navigating from the RSVP page would retain 'overflow:hidden' on the body.

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,14 @@
+<script setup lang="ts">
+onMounted(() => {
+    // Typeform sets `overflow: hidden` on `body` when a form is mounted, and that style is retained when moving to another page.
+    // This fixes that problem by removing the style.
+    const body = document.querySelector("body");
+    if (body) {
+        body.style.cssText = "";
+    }
+});
+</script>
+
 <template>
     <div>
         <Hero />
--- pages/registry.vue
+++ pages/registry.vue
@@ -1,5 +1,13 @@
 <script setup>
 onMounted(() => {
+    // Typeform sets `overflow: hidden` on `body` when a form is mounted, and that style is retained when moving to another page.
+    // This fixes that problem by removing the style.
+    const body = document.querySelector("body");
+    if (body) {
+        body.style.cssText = "";
+    }
+
+    // This embeds the Zola registry.
     !function (e, t, n) { var s, a = e.getElementsByTagName(t)[0]; e.getElementById(n) || (s = e.createElement(t), s.id = n, s.async = !0, s.src = "https://widget.zola.com/js/widget.js", a.parentNode.insertBefore(s, a)) }(document, "script", "zola-wjs");
 });
 </script>
