67812fbba8a0f37a0983c8b6c41bdcbf6abd2234
Richard <heyrichardshi@users.noreply.github.com>
2023-03-07 20:46:51 -0500

    Added logo to the footer and fixed spacing.

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,8 @@
 <template>
-    <div class="grid grid-cols-1 gap-8 mx-4 lg:mx-40 h-24 lg:h-40 text-center content-center justify-items-center">
+    <div class="grid grid-cols-1 gap-8 lg:gap-16 mx-4 lg:mx-40 py-8 lg:py-16 text-center content-center justify-items-center">
+        <div>
+            <img src="~/assets/images/logo.png" class="m-auto w-4/5 lg:w-auto lg:h-64" />
+        </div>
         <div class="leading-loose font-serif italic">Made with 💕 by Richard & Na.</div>
     </div>
 </template>
